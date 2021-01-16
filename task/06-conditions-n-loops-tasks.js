02ace21cc59d171974efad8cd0ccb2dffe5130db
Oleg Yarotskiy <oleg.yarotskiy.99@gmail.com>
2021-01-16 22:44:30 +0300

    06 isInsideCircle issue solved

--- task/06-conditions-n-loops-tasks.js
+++ task/06-conditions-n-loops-tasks.js
@@ -167,11 +167,7 @@ function doRectanglesOverlap(rect1, rect2) {
  */
 function isInsideCircle(circle, point) {
     let length = Math.sqrt(Math.pow((point.x - circle.center.x), 2) + Math.pow((point.y - circle.center.y), 2));
-    if (length < circle.radius) {
-        return true;
-    } else {
-        return false;
-    }
+    return length < circle.radius;
 }
 
 
