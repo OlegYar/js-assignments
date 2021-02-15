c698f354cf59f2bb50503b8721b9ad8432b1c8cc
Oleg <oleg.yarotskiy.99@gmail.com>
2021-02-15 21:46:17 +0300

    04 getMovingSum used reduce

--- task/04-arrays-tasks.js
+++ task/04-arrays-tasks.js
@@ -254,8 +254,7 @@ function toArrayOfSquares(arr) {
  *   [ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 ] => [ 1, 3, 6, 10, 15, 21, 28, 36, 45, 55 ]
  */
 function getMovingSum(arr) {
-   let sum = 0;
-   return arr.map(item => sum += item);
+   return arr.reduce((pr, item, index) => index ? pr.concat(item + pr[index - 1]) : pr.concat(item), []);
 }
 
 /**
